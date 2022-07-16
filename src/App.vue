dd5eb894ad105016ed007ede8971b6ed4dff75a2
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-16 16:36:32 +0800

    Feat: Add TheNagivation component skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
+  <TheNagivation />
+
   <main>
     <router-view></router-view>
   </main>
 </template>
+
+<script setup>
+import TheNagivation from "./components/TheNagivation.vue";
+</script>
---
+++ src/components/TheNagivation.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/"
+          ><span aria-hidden="true">00</span> Home</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/"
+          ><span aria-hidden="true">01</span> Destination</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/"
+          ><span aria-hidden="true">02</span> Crew</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/"
+          ><span aria-hidden="true">03</span> Technology</router-link
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
