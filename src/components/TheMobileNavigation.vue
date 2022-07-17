64be76dfe79dae52591b20a5578e56a67f6ca556
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-17 20:02:53 +0800

    Feat: Finish TheNavigation components
    - establish breakpoints for tablet and mobile
    - create new component TheMobileNagivation for teleport in Vue
    - add font-weight bold for Barlow Condensed
    - place reusable navbar styles in main and unique classes inside TheNagivation and TheMobileNagivation components

---
+++ src/components/TheMobileNavigation.vue
@@ -0,0 +1,140 @@
+<template>
+  <transition name="slide">
+    <div v-if="props.show" class="navbar navbar--mobile">
+      <button
+        @click="$emit('closeModal')"
+        aria-label="close mobile nagivation"
+        type="button"
+        class="navbar--mobile__close"
+      >
+        <svg
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="21"
+        >
+          <g fill="#D0D6F9" fill-rule="evenodd">
+            <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
+            <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
+          </g>
+        </svg>
+      </button>
+
+      <ul class="navbar__link-list navbar__link-list--mobile">
+        <li>
+          <router-link to="/"
+            ><span aria-hidden="true">00</span>Home</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/destination"
+            ><span aria-hidden="true">01</span>Destination</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/crew"
+            ><span aria-hidden="true">02</span>Crew</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/technology"
+            ><span aria-hidden="true">03</span>Technology</router-link
+          >
+        </li>
+      </ul>
+    </div>
+  </transition>
+</template>
+
+<script setup>
+const props = defineProps({
+  show: {
+    type: Boolean,
+    required: true,
+  },
+});
+</script>
+
+<style>
+.navbar--mobile {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 25.4rem;
+  height: 100vh;
+
+  background-color: var(--clr-black);
+  z-index: 999;
+}
+
+@supports (backdrop-filter: blur) {
+  .navbar--mobile {
+    background-color: rgba(255, 255, 255, 0.04);
+    backdrop-filter: blur(81.5485px);
+  }
+}
+
+.navbar--mobile__close {
+  position: absolute;
+  top: 3.4rem;
+  right: 2.4rem;
+
+  border-bottom: 2px solid transparent;
+  transition: border-color 0.2s ease-in-out;
+}
+
+.navbar--mobile__close:hover,
+.navbar--mobile__close:focus {
+  border-color: white;
+}
+
+.navbar__link-list--mobile {
+  align-self: flex-start;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  gap: 3.2rem;
+  margin-block-start: 6.5rem;
+  padding-inline: 3.2rem;
+}
+
+.navbar__link-list--mobile li a::after {
+  position: absolute;
+  left: -1.6rem;
+  top: 0;
+  transform: scaleY(0);
+  transform-origin: center;
+  content: "";
+  width: 0.3rem;
+  height: 100%;
+
+  background-color: var(--clr-white);
+  opacity: 0.5;
+  transition: transform 0.2s ease-in-out;
+}
+
+.navbar__link-list--mobile li a.router-link-active::after {
+  opacity: 1;
+  transform: scaleY(1);
+}
+
+.navbar__link-list--mobile li a:hover::after,
+.navbar__link-list--mobile li a:focus::after {
+  transform: scaleY(1);
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.4s ease-in-out;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateX(100%);
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  transform: translateX(0);
+}
+</style>
--- src/components/TheNagivation.vue
+++ src/components/TheNagivation.vue
@@ -1,6 +1,7 @@
 <template>
   <nav class="navbar">
     <svg
+      class="navbar__logo"
       xmlns="http://www.w3.org/2000/svg"
       width="48"
       height="48"
@@ -15,7 +16,7 @@
       </g>
     </svg>
 
-    <ul class="navbar__link-list">
+    <ul class="navbar__link-list navbar__link-list--desktop">
       <li>
         <router-link to="/"><span aria-hidden="true">00</span>Home</router-link>
       </li>
@@ -35,70 +36,180 @@
         >
       </li>
     </ul>
+
+    <button
+      @click="handleOpenModal"
+      class="navbar__burger"
+      aria-label="open mobile navigation"
+      type="button"
+    >
+      <svg
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="21"
+      >
+        <g fill="#D0D6F9" fill-rule="evenodd">
+          <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
+        </g>
+      </svg>
+    </button>
+
+    <Teleport to="body">
+      <TheMobileNavigation
+        @close-modal="handleCloseModal"
+        :show="showMobileNav"
+      />
+    </Teleport>
   </nav>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import TheMobileNavigation from "./TheMobileNavigation.vue";
+
+const showMobileNav = ref(false);
+
+function handleCloseModal() {
+  showMobileNav.value = false;
+}
+
+function handleOpenModal() {
+  showMobileNav.value = true;
+}
+</script>
 
 <style>
 .navbar {
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  margin-block-start: 4rem;
-  padding-inline-start: 5.5rem;
+  padding: 2.4rem;
 }
 
-.navbar__link-list {
-  display: flex;
-  align-items: stretch;
-  justify-content: center;
-  gap: 4.8rem;
-  max-width: 83rem;
-  min-height: 9.6rem;
-  padding-inline: 12.3rem 16.5rem;
-  background-color: rgba(255, 255, 255, 0.04);
-  backdrop-filter: blur(81.5485px);
+.navbar__logo {
+  transform: scale(0.83);
 }
 
-.navbar__link-list li {
-  display: flex;
-  align-items: stretch;
-  padding-block-start: var(--padding-top, 3.8rem);
-}
-
-.navbar__link-list li a {
-  position: relative;
-  outline: none;
-}
-
-.navbar__link-list li a::after {
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  height: 0.3rem;
-  transform: scaleX(0);
-  background-color: var(--clr-white);
-  opacity: 0.5;
-
-  content: "";
+.navbar__burger {
   transition: transform 0.2s ease-in-out;
-  transform-origin: center;
 }
 
-.navbar__link-list li a.router-link-active::after {
-  opacity: 1;
-  transform: scaleX(1);
+.navbar__burger:hover,
+.navbar__burger:focus {
+  transform: scale(0.8) rotate(45deg);
 }
 
-.navbar__link-list li a:hover::after,
-.navbar__link-list li a:focus::after {
-  transform: scaleX(1);
+@media screen and (min-width: 40.625em) {
+  .navbar {
+    --margin-top: 0;
+    --padding-left: 3.9rem;
+    --font-size: 1.4rem;
+    --letter-spacing: 2.7px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    font-size: var(--font-size);
+    letter-spacing: var(--letter-spacing);
+
+    margin-block-start: var(--margin-top);
+    padding: 0;
+    padding-inline-start: var(--padding-left);
+  }
+
+  .navbar__logo {
+    transform: scale(1);
+  }
+
+  .navbar__burger {
+    display: none;
+  }
 }
 
-.navbar__link-list li a span {
-  font-weight: 700;
-  letter-spacing: 2.7px;
-  margin-inline-end: 1.2rem;
+@media screen and (min-width: 62.5em) {
+  .navbar {
+    --margin-top: 4rem;
+    --padding-left: 5.5rem;
+    --font-size: 1.6rem;
+    --letter-spacing: 2.3625px;
+  }
+}
+
+.navbar__link-list--desktop {
+  display: none;
+}
+
+@media screen and (min-width: 40.625em) {
+  .navbar__link-list--desktop {
+    --width: 45rem;
+    --padding-left: 4.8rem;
+    --padding-right: 4.6rem;
+
+    display: flex;
+    align-items: stretch;
+    justify-content: space-between;
+    width: var(--width);
+    min-height: 9.6rem;
+    padding-inline: var(--padding-left) var(--padding-right);
+    background-color: rgba(255, 255, 255, 0.04);
+    backdrop-filter: blur(81.5485px);
+  }
+
+  .navbar__link-list--desktop li {
+    display: flex;
+    align-items: stretch;
+    padding-block-start: 3.8rem;
+  }
+
+  .navbar__link-list--desktop li a {
+    position: relative;
+    outline: none;
+  }
+
+  .navbar__link-list--desktop li a::after {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 0.3rem;
+    transform: scaleX(0);
+    background-color: var(--clr-white);
+    opacity: 0.5;
+
+    content: "";
+    transition: transform 0.2s ease-in-out;
+    transform-origin: center;
+  }
+
+  .navbar__link-list--desktop li a.router-link-active::after {
+    opacity: 1;
+    transform: scaleX(1);
+  }
+
+  .navbar__link-list--desktop li a:hover::after,
+  .navbar__link-list--desktop li a:focus::after {
+    transform: scaleX(1);
+  }
+
+  .navbar__link-list--desktop li a span {
+    display: none;
+  }
+}
+
+@media screen and (min-width: 62.5em) {
+  .navbar__link-list--desktop {
+    --width: 83rem;
+    --padding-left: 12.3rem;
+    --padding-right: 16.5rem;
+    --gap: 4.8rem;
+  }
+}
+
+@media screen and (min-width: 62.5em) {
+  .navbar__link-list--desktop li a span {
+    display: inline;
+  }
 }
 </style>
