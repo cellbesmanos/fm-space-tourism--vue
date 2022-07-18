df931dd389212c52bd5d99c1415f047eaa4b2f40
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-18 17:30:39 +0800

    Fix: Add unique key for router-view

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
   <TheNagivation />
 
   <main>
-    <router-view></router-view>
+    <router-view :key="route.path"></router-view>
   </main>
 </template>
 
 <script setup>
+import { useRoute } from "vue-router";
 import TheNagivation from "./components/TheNagivation.vue";
+
+const route = useRoute();
 </script>
